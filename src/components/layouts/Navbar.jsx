06cc8005ab7e54194f5b885d25d9e5dec4cdad31
Gabriel Almeida <gabrielalmeida7258@gmail.com>
2025-01-10 09:43:30 -0300

    Navbar funcionando, mas com rolagem no conteúdo

--- src/components/layouts/Navbar.jsx
+++ src/components/layouts/Navbar.jsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom'
+import { Link, useLocation, useNavigate } from 'react-router-dom'
 import { useState, useEffect } from 'react';
 
 import styles from './Navbar.module.css'
@@ -8,32 +8,57 @@ import { AiOutlineMenuFold, AiOutlineMenuUnfold } from "react-icons/ai";
 import logo from '../../images/logo.webp'
 
 function Navbar() {
-
+    const navigate = useNavigate();
+    const location = useLocation();
     const [isMenuVisible, setIsMenuVisible] = useState(false);
+
+    function toggleMenuVisibility() {
+        setIsMenuVisible(!isMenuVisible);
+    }
+
+    function handleInternalLink(e, sectionId) {
+        e.preventDefault();
+
+        if (location.pathname !== "/my-portfolio") {
+            navigate("/my-portfolio", { state: { scrollTo: sectionId } });
+        }
+        else {
+            const section = document.getElementById(sectionId);
+            if (section) section.scrollIntoView({ behavior: "smooth" });
+        }
+    }
+    useEffect(() => {
+        if (location.state?.scrollTo) {
+            const section = document.getElementById(location.state.scrollTo);
+            if (section) section.scrollIntoView({ behavior: "smooth" });
+        }
+    }, [location])
 
     useEffect(() => {
         
         if (isMenuVisible) {
             document.body.classList.add("menuVisible");
+            document.documentElement.classList.add("menuVisible");
         }
         else {
             document.body.classList.remove("menuVisible");
+            document.documentElement.classList.remove("menuVisible");
+
         }
     }, [isMenuVisible]);
-    
-    function toggleMenuVisibility() {
-        setIsMenuVisible(!isMenuVisible);
-    }
 
     return (<>
-        <nav className={styles.navbar}>
+        <nav className={styles.navbar} aria-label='Navegação principal'>
             <div className={styles.logo}>
                 <Link to="/my-portfolio">
                     <img src={logo} alt="Gabriel Almeyda" />
                 </Link>
             </div>
 
-            <label onClick={toggleMenuVisibility}>
+            <label onClick={toggleMenuVisibility}
+                aria-label='Alternar visibilidade do menu'
+                aria-expanded={isMenuVisible}
+            >
                 {isMenuVisible ? (
                     <AiOutlineMenuUnfold />
                 ) : (
@@ -41,18 +66,26 @@ function Navbar() {
                 )}
             </label>
         
-            <ul className={isMenuVisible ? styles.visible : styles.hidden}>
+            <ul className={isMenuVisible ? styles.visible : styles.hidden}
+                role='Menu'
+            >
                 <li>
-                    <a href="#about">About me</a>
+                    <a href='#about' onClick={(e) => {
+                        handleInternalLink(e, "about");
+                        toggleMenuVisibility();
+                    }}>Sobre</a>
                 </li>
                 <li>
-                    <a href="#tools">Tools</a>
+                    <a href='#tools' onClick={(e) => {
+                        handleInternalLink(e, "tools");
+                        toggleMenuVisibility();
+                    }}>Ferramentas</a>
                 </li>
                 <li>
-                    <Link to="/my-portfolio/projects">Projects</Link>
+                    <Link to="/my-portfolio/projects">Projetos</Link>
                 </li>
                 <li>
-                    <Link to="/my-portfolio/contact">Contact</Link>
+                    <Link to="/my-portfolio/contact">Contato</Link>
                 </li>
             </ul>
         </nav>
